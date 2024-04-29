e8ee5537106efb382ecc9d5351d0b0b0a0b293da
Rushikesh <es20btech11022@iith.ac.in>
2024-04-29 19:42:43 +0530

    Magnetic Field Caliberation Code added for Roof and Telescope Module

--- RoofModule/sender1.ino
+++ RoofModule/sender1.ino
@@ -19,11 +19,31 @@ void setup()
     Serial.begin(115200);
     delay(10);
 
+
     // We start by connecting to a WiFi network
-    WiFiMulti.addAP("RUSHI", "23112002");
+   
+    compass.init();
+    compass.calibrate();
+    Serial.println();
+    Serial.print("compass.setCalibrationOffsets(");
+    Serial.print(compass.getCalibrationOffset(0));
+    Serial.print(", ");
+    Serial.print(compass.getCalibrationOffset(1));
+    Serial.print(", ");
+    Serial.print(compass.getCalibrationOffset(2));
+    Serial.println(");");
+    Serial.print("compass.setCalibrationScales(");
+    Serial.print(compass.getCalibrationScale(0));
+    Serial.print(", ");
+    Serial.print(compass.getCalibrationScale(1));
+    Serial.print(", ");
+    Serial.print(compass.getCalibrationScale(2));
+    Serial.println(");");
+    
 
     Serial.println();
     Serial.println();
+    WiFiMulti.addAP("RUSHI", "23112002");
     Serial.print("Waiting for WiFi... ");
 
     while(WiFiMulti.run() != WL_CONNECTED) {
@@ -40,9 +60,7 @@ void setup()
 
 
   
-  Serial.begin(9600);
   // Initialize device with i2c 0x0D address
-  compass.init();
   delay(100);
 }
 
@@ -105,10 +123,34 @@ void loop()
       if(curr_millis-prev_millis > 500){
         Serial.print(String(angle)+"\n");
         // GET /index.html HTTP/1.1\n\n
-        client.print("GET /sender=telescope/angle="+String(angle)+"/ HTTP/1.1\n\n");
+        client.print("GET /sender=roof/angle="+String(angle)+"/ HTTP/1.1\n\n");
         prev_millis = curr_millis;
       }
       // break;
     }
     delay(3000);
+  // int maxloops = 0;
+
+  // //wait for the server's reply to become available
+  // while (!client.available() && maxloops < 1000)
+  // {
+  //   maxloops++;
+  //   delay(1); //delay 1 msec
+  // }
+  // if (client.available() > 0)
+  // {
+  //   //read back one line from the server
+  //   String line = client.readStringUntil('\r');
+  //   Serial.println(line);
+  // }
+  // else
+  // {
+  //   Serial.println("client.available() timed out ");
+  // }
+
+  //   Serial.println("Closing connection.");
+  //   client.stop();
+
+  //   Serial.println("Waiting 5 seconds before restarting...");
+  //   delay(5000);
 }
